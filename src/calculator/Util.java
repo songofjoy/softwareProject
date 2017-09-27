490a5140a67d7025080ad829f13daefad0a8d633
hhs <Song@Lenovo-PC>
2017-09-27 09:54:01 +0800

    计算器 GUI

--- src/calculator/Util.java
+++ src/calculator/Util.java
@@ -1,5 +1,47 @@
 package calculator;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+
 public class Util {
 
+	/**
+	 * 读取文本文件
+	 */
+	public static String readTxtFile(String filePath) {
+
+		String ret = "";
+		try {
+			String encoding = "Unicode";
+			File file = new File(filePath);
+			if (file.isFile() && file.exists()) { // 判断文件是否存在
+				InputStreamReader read = new InputStreamReader(
+						new FileInputStream(file), encoding);// 考虑到编码格式
+				BufferedReader bufferedReader = new BufferedReader(read);
+				String lineTxt = null;
+				while ((lineTxt = bufferedReader.readLine()) != null) {
+					ret = ret + lineTxt;
+				}
+				read.close();
+			} else {
+				System.out.println("找不到指定的文件");
+			}
+		} catch (Exception e) {
+			System.out.println("读取文件内容出错");
+			e.printStackTrace();
+		}
+		return ret;
+	}
+
+	/**
+	 * 
+	 * 获取目录下的所有文件名 By Hhs
+	 */
+	public static String[] getFileName(String path) {
+		File file = new File(path);
+		String[] fileName = file.list();
+		return fileName;
+	}
 }
