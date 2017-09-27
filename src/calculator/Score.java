f9c3ae534222f300ac2c90e031f58abe7b867a67
hhs <Song@Lenovo-PC>
2017-09-27 11:41:34 +0800

    中英切换

---
+++ src/calculator/Score.java
@@ -0,0 +1,29 @@
+package calculator;
+
+public class Score {
+
+	private int rightAmount;
+	private int wrongAmount;
+	private double radioAmount;
+	
+	public int getRightAmount() {
+		return rightAmount;
+	}
+	public void setRightAmount(int rightAmount) {
+		this.rightAmount = rightAmount;
+	}
+	public int getWrongAmount() {
+		return wrongAmount;
+	}
+	public void setWrongAmount(int wrongAmount) {
+		this.wrongAmount = wrongAmount;
+	}
+	public double getRadioAmount() {
+		return radioAmount;
+	}
+	public void setRadioAmount(double radioAmount) {
+		this.radioAmount = radioAmount;
+	}
+	
+	
+}
